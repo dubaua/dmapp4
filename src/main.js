e723470686537508e20a09362a5526c8ef666601
dubaua <dubaua@gmail.com>
2019-02-26 15:57:12 +0500

    add pt root ui web font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/components/globals";
+import "@/assets/fonts/pt_root_ui/pt_root_ui.css";
 
 Vue.config.productionTip = false;
 
